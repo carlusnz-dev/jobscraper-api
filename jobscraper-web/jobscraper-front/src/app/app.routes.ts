28bfdaf076bd8f876ec024ef5e914e9b4580b945
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-10-04 17:04:36 -0300

    Removed folder jobscraper-web/ and moved to features branch

--- jobscraper-web/jobscraper-front/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { NotFound } from './pages/not-found/not-found';
-import { HomePage } from './pages/home-page/home-page';
-
-export const routes: Routes = [
-  { path: '', component: HomePage},
-  { path: 'not-found', component: NotFound },
-  { path: '**', redirectTo: '/not-found'}
-];
