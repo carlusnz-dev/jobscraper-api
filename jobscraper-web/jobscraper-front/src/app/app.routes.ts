8ea00300d009119caa2b34399a8c77c1e3cf7d81
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-10-07 19:09:03 -0300

    update about page and add contact page

--- jobscraper-web/jobscraper-front/src/app/app.routes.ts
+++ jobscraper-web/jobscraper-front/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { NotFound } from './pages/not-found/not-found';
 import { HomePage } from './pages/home-page/home-page';
 import { AboutPage } from './pages/about-page/about-page';
+import { ContactPage } from './pages/contact-page/contact-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'about', component: AboutPage },
+  { path: 'contact', component: ContactPage },
   { path: 'not-found', component: NotFound },
   { path: '**', redirectTo: '/not-found'}
 ];
