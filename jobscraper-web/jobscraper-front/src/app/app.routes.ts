1ff106821952b1baa828fd62ab213a8962fce7b9
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-10-07 18:56:56 -0300

    update home page style and add about page

--- jobscraper-web/jobscraper-front/src/app/app.routes.ts
+++ jobscraper-web/jobscraper-front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './pages/not-found/not-found';
 import { HomePage } from './pages/home-page/home-page';
+import { AboutPage } from './pages/about-page/about-page';
 
 export const routes: Routes = [
-  { path: '', component: HomePage},
+  { path: '', component: HomePage },
+  { path: 'about', component: AboutPage },
   { path: 'not-found', component: NotFound },
   { path: '**', redirectTo: '/not-found'}
 ];
--- jobscraper-web/jobscraper-front/src/app/pages/home-page/home-page.ts
+++ jobscraper-web/jobscraper-front/src/app/pages/home-page/home-page.ts
@@ -1,12 +1,95 @@
 import { Component } from '@angular/core';
-import { SearchComponent } from "../../components/search-component/search-component";
+import { SearchComponent } from '../../components/search-component/search-component';
 
 @Component({
   selector: 'app-home-page',
   imports: [SearchComponent],
   templateUrl: './home-page.html',
-  styleUrl: './home-page.css'
+  styleUrl: './home-page.css',
 })
 export class HomePage {
+  hourToday = new Date();
 
+  jobFounds = [
+    {
+      id: 1,
+      title: 'JobFound 01',
+      companion: 'company 01',
+      type: 'full-time',
+      occupation: 'java & spring boot',
+      description: 'loremispum',
+    },
+    {
+      id: 2,
+      title: 'JobFound 02',
+      companion: 'company 02',
+      type: 'part-time',
+      occupation: 'python & django',
+      description: 'loremispum',
+    },
+    {
+      id: 3,
+      title: 'JobFound 03',
+      companion: 'company 03',
+      type: 'contract',
+      occupation: 'javascript & react',
+      description: 'loremispum',
+    },
+    {
+      id: 4,
+      title: 'JobFound 04',
+      companion: 'company 04',
+      type: 'internship',
+      occupation: 'c# & .net',
+      description: 'loremispum',
+    },
+    {
+      id: 5,
+      title: 'JobFound 05',
+      companion: 'company 05',
+      type: 'remote',
+      occupation: 'php & laravel',
+      description: 'loremispum',
+    },
+    {
+      id: 6,
+      title: 'JobFound 06',
+      companion: 'company 06',
+      type: 'full-time',
+      occupation: 'ruby & rails',
+      description: 'loremispum',
+    },
+    {
+      id: 7,
+      title: 'JobFound 07',
+      companion: 'company 07',
+      type: 'part-time',
+      occupation: 'go & gin',
+      description: 'loremispum',
+    },
+    {
+      id: 8,
+      title: 'JobFound 08',
+      companion: 'company 08',
+      type: 'contract',
+      occupation: 'typescript & angular',
+      description: 'loremispum',
+    },
+    {
+      id: 9,
+      title: 'JobFound 09',
+      companion: 'company 09',
+      type: 'internship',
+      occupation: 'swift & ios',
+      description: 'loremispum',
+    },
+    {
+      id: 10,
+      title: 'JobFound 10',
+      companion: 'company 10',
+      type: 'remote',
+      occupation: 'kotlin & android',
+      description: 'loremispum',
+    },
+  ];
 }
